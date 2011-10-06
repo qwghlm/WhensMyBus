936970ba769b76ba7516462c6dcd4a281d71585b
Chris Applegate <github@qwghlm.co.uk>
2011-10-06 14:06:09 +0100

    More unit tests

--- testing.py
+++ testing.py
@@ -10,16 +10,19 @@ if sys.version_info < (2, 7):
     sys.exit(1)    
 
 from whensmybus import WhensMyBus, WhensMyBusException
+import re
 import unittest
 
 class FakeTweet:
     """
     Fake Tweet object to simulate tweepy's Tweet object being passed to various functions
     """
-    def __init__(self, text):
+    def __init__(self, text, longitude=None, latitude=None, place=None):
         self.user = lambda:1
         self.user.screen_name = 'testuser'
         self.text = text
+        self.place = place
+        self.coordinates = None
 
 class WhensMyBusTestCase(unittest.TestCase):
     """
@@ -45,7 +48,7 @@ class WhensMyBusTestCase(unittest.TestCase):
         """
         Test to see if we can load the class in the first place
         """
-        self.assertTrue(True, 'Script cannot start up')
+        self.assertTrue(True)
         
     def test_database(self):
         """
@@ -67,7 +70,7 @@ class WhensMyBusTestCase(unittest.TestCase):
         """
         Test to see if our OAuth login details are correct
         """
-        self.assertTrue(self.wmb.api.verify_credentials(), 'OAuth settings incorrect')
+        self.assertTrue(self.wmb.api.verify_credentials())
         
     #
     # Really important tests
@@ -78,14 +81,34 @@ class WhensMyBusTestCase(unittest.TestCase):
         Test to confirm we are ignoring Tweets that are just mentions and not replies
         """
         tweet = FakeTweet('Hello @whensmybus')
-        self.assertFalse(self.wmb.process_tweet(tweet), 'Incorrectly replying to non-reply mentions')
+        self.assertFalse(self.wmb.process_tweet(tweet))
 
     def test_no_bus_number(self):
         """
         Test to confirm we are ignoring Tweets that do not have bus numbers in them
         """
         tweet = FakeTweet('@whensmybus Thanks!')
-        self.assertFalse(self.wmb.process_tweet(tweet), 'Incorrectly replying to mentions without bus number')
+        self.assertFalse(self.wmb.process_tweet(tweet))
+
+    def _test_correct_exception_produced(self, tweet, exception_id, *string_params):
+        """
+        Generic test for the correct exception message
+        """
+        # Get the message for the exception_id specified, applying C string formatting if applicable
+        # Then escape it so that regular expression module doesn't accidentally interpret brackets etc
+        expected_error = re.escape(WhensMyBusException.exception_values[exception_id] % string_params)
+        
+        # Match the expected Exception message against the actual Exception raised when we try to process Tweet
+        self.assertRaisesRegexp(WhensMyBusException, expected_error, self.wmb.process_tweet, tweet)    
+        
+    def test_nonexistent_bus(self):
+        tweet = FakeTweet('@whensmybus 218')
+        self._test_correct_exception_produced(tweet, 'nonexistent_bus', '218')
+        
+    def test_no_geotag(self):
+        tweet = FakeTweet('@whensmybus 15')
+        self._test_correct_exception_produced(tweet, 'no_geotag')
+    
    
 # @whensmybus 15
 # @whensmybus  15
@@ -98,6 +121,6 @@ results = unittest.TextTestRunner(verbosity=1, failfast=1).run(suite)
 
 # If we pass, then run tests on individual functionality
 if not (results.failures + results.errors):
-    main_tests = ('mention','no_bus_number')
+    main_tests = ('mention','no_bus_number','nonexistent_bus','no_geotag')
     suite = unittest.TestSuite(map(WhensMyBusTestCase, ['test_%s' % t for t in main_tests]))
     results = unittest.TextTestRunner(verbosity=1).run(suite)
