87b938faa205f45629ca6986f8da259134b1d2d9
Chris Applegate <github@qwghlm.co.uk>
2011-11-24 10:50:56 +0000

    More unit testing for DMs

--- testing.py
+++ testing.py
@@ -53,14 +53,16 @@ class WhensMyBusTestCase(unittest.TestCase):
         """
         self.wmb = WhensMyBus(testing=True, silent=True)
         
-        self.test_tweets = (('@%s %s', '15'),)
+        self.at_reply = '@%s ' % self.wmb.username
         
-        self.test_tweets_with_ids = (('@%s %s from 52240', '277'),)
+        self.test_messages = (('%s', '15'),)
+        
+        self.test_messages_with_ids = (('%s from 52240', '277'),)
 
-        self.test_tweets_with_locations = (('@%s %s from Hoxton', '243'),
-                                           ('@%s %s from Hoxton station', '243'),
+        self.test_messages_with_locations = (('%s from Hoxton', '243'),
+                                            ('%s from Hoxton station', '243'),
                                            #('@%s %s from E2 8DY', '243'),
-                                          )
+                                            )
 
     def tearDown(self):
         """
@@ -115,16 +117,17 @@ class WhensMyBusTestCase(unittest.TestCase):
         """
         Test to confirm we are ignoring Tweets from the bot itself
         """
-        tweet = FakeTweet('@%s 15' % self.wmb.username, username=self.wmb.username)
+        tweet = FakeTweet(self.at_reply + '15', username=self.wmb.username)
         self.assertFalse(self.wmb.validate_tweet(tweet))
 
     def test_no_bus_number(self):
         """
         Test to confirm we are ignoring Tweets that do not have bus numbers in them
         """
-        tweet = FakeTweet('@%s Thanks!' % self.wmb.username)
+        message = 'Thanks!'
+        tweet = FakeTweet(self.at_reply + message)
         self.assertFalse(self.wmb.process_tweet(tweet))
-        dm = FakeDirectMessage('Thanks!')
+        dm = FakeDirectMessage(message)
         self.assertFalse(self.wmb.process_tweet(dm))
 
     def _test_correct_exception_produced(self, tweet, exception_id, *string_params):
@@ -142,28 +145,22 @@ class WhensMyBusTestCase(unittest.TestCase):
         """
         Test to confirm we are ignoring blank replies
         """
-        for text in ('@%s' % self.wmb.username,
-                     '@%s ' % self.wmb.username,
-                     '@%s         ' % self.wmb.username,):
-            tweet = FakeTweet(text)
+        for message in ('',
+                     ' ',
+                     '         '):
+            tweet = FakeTweet(self.at_reply + message)
             self._test_correct_exception_produced(tweet, 'blank_tweet')
-        for text in ('', ' ', '      '):
-            dm = FakeDirectMessage(text)
+            dm = FakeDirectMessage(message)
             self._test_correct_exception_produced(dm, 'blank_tweet')
 
     def test_nonexistent_bus(self):
         """
         Test to confirm non-existent buses handled OK
         """
-        for text in ('@%s 218' % self.wmb.username,
-                     '@%s    218' % self.wmb.username,
-                     '@%s    218   #hashtag' % self.wmb.username,):
-            tweet = FakeTweet(text)
+        for message in ('218', '   218', '   218   #hashtag'):
+            tweet = FakeTweet(self.at_reply + message)
             self._test_correct_exception_produced(tweet, 'nonexistent_bus', '218')
-        for text in ('218',
-                     '   218',
-                     '   218   #hashtag'):
-            dm = FakeDirectMessage(text)
+            dm = FakeDirectMessage(message)
             self._test_correct_exception_produced(dm, 'nonexistent_bus', '218')
 
 
@@ -171,57 +168,67 @@ class WhensMyBusTestCase(unittest.TestCase):
         """
         Test to confirm lack of geotag handled OK
         """
-        for (text, route) in self.test_tweets:
-            tweet = FakeTweet(text % (self.wmb.username, route))
+        for (text, route) in self.test_messages:
+            message = text % route
+            tweet = FakeTweet(self.at_reply + message)
             self._test_correct_exception_produced(tweet, 'no_geotag', route)
-        for (text, route) in self.test_tweets:
-            dm = FakeDirectMessage(route)
+            dm = FakeDirectMessage(message)
             self._test_correct_exception_produced(dm, 'dms_not_taggable', route)
 
     def test_placeinfo_only(self):
         """
         Test to confirm ambiguous place information handled OK
         """
-        for (text, route) in self.test_tweets:
-            tweet = FakeTweet(text % (self.wmb.username, route), place='foo')
+        for (text, route) in self.test_messages:
+            message = text % route
+            tweet = FakeTweet(self.at_reply + message, place='foo')
             self._test_correct_exception_produced(tweet, 'placeinfo_only', route)
             
     def test_not_in_uk(self):
         """
         Test to confirm geolocations outside UK handled OK
         """
-        for (text, route) in self.test_tweets:
-            tweet = FakeTweet(text % (self.wmb.username, route), -73.985656, 40.748433) # Empire State Building, New York
+        for (text, route) in self.test_messages:
+            message = text % route
+            tweet = FakeTweet(self.at_reply + message, -73.985656, 40.748433) # Empire State Building, New York
             self._test_correct_exception_produced(tweet, 'not_in_uk')
 
     def test_not_in_london(self):
         """
         Test to confirm geolocations outside London handled OK
         """
-        for (text, route) in self.test_tweets:
-            tweet = FakeTweet(text % (self.wmb.username, route), -3.200833, 55.948611) # Edinburgh Castle, Edinburgh
+        for (text, route) in self.test_messages:
+            message = text % route
+            tweet = FakeTweet(self.at_reply + message, -3.200833, 55.948611) # Edinburgh Castle, Edinburgh
             self._test_correct_exception_produced(tweet, 'not_in_london')
 
     def test_bad_stop_id(self):
         """
         Test to confirm bad stop IDs handled OK
         """
-        tweet = FakeTweet('@%s 15 from 00000' % (self.wmb.username,)) 
+        message = '15 from 00000'
+        tweet = FakeTweet(self.at_reply + message) 
         self._test_correct_exception_produced(tweet, 'bad_stop_id', '00000') # Stop IDs begin at 47000
+        dm = FakeDirectMessage(message) 
+        self._test_correct_exception_produced(dm, 'bad_stop_id', '00000') # Stop IDs begin at 47000
         
     def test_stop_id_mismatch(self):
         """
         Test to confirm when route and stop do not match up is handled OK
         """
-        tweet = FakeTweet('@%s 15 from 52240' % (self.wmb.username,)) 
+        message = '15 from 52240'
+        tweet = FakeTweet(self.at_reply + message) 
         self._test_correct_exception_produced(tweet, 'stop_id_mismatch', '15', '52240') # The 15 does not go from Canary Wharf
+        dm = FakeDirectMessage(message) 
+        self._test_correct_exception_produced(dm, 'stop_id_mismatch', '15', '52240') # Stop IDs begin at 47000
     
     def test_in_london_with_geotag(self):
         """
         Test to confirm a correctly-geotagged message is handled OK
         """
-        for (text, route) in self.test_tweets:
-            tweet = FakeTweet(text % (self.wmb.username, route), -0.0397, 51.5124) # Limehouse Station, London
+        for (text, route) in self.test_messages:
+            message = text % route
+            tweet = FakeTweet(self.at_reply + message, -0.0397, 51.5124) # Limehouse Station, London
             result = self.wmb.process_tweet(tweet)
 
             for unwanted in ('LIMEHOUSE STATION', '<>', '#', '\[DLR\]', '>T<'):                
@@ -234,8 +241,9 @@ class WhensMyBusTestCase(unittest.TestCase):
         """
         Test to confirm a message with correct stop ID is handled OK
         """
-        for (text, route) in self.test_tweets_with_ids:
-            tweet = FakeTweet(text % (self.wmb.username, route))
+        for (text, route) in self.test_messages_with_ids:
+            message = text % route
+            tweet = FakeTweet(self.at_reply + message)
             result = self.wmb.process_tweet(tweet)
 
             for unwanted in ('CANARY WHARF', '<>', '#', '\[DLR\]', '>T<'):                
@@ -243,14 +251,33 @@ class WhensMyBusTestCase(unittest.TestCase):
 
             self.assertRegexpMatches(result, route.upper())
             self.assertRegexpMatches(result, '(Canary Wharf Station to .* [0-9]{4}|None shown going)')
+            
+            dm = FakeDirectMessage(message)
+            result = self.wmb.process_tweet(dm)
+            for unwanted in ('CANARY WHARF', '<>', '#', '\[DLR\]', '>T<'):                
+                self.assertNotRegexpMatches(result, unwanted)
+
+            self.assertRegexpMatches(result, route.upper())
+            self.assertRegexpMatches(result, '(Canary Wharf Station to .* [0-9]{4}|None shown going)')            
+            
 
     def test_in_london_with_stop_locations(self):
         """
         Test to confirm a message with location name is handled OK
         """
-        for (text, route) in self.test_tweets_with_locations:
-            tweet = FakeTweet(text % (self.wmb.username, route))
+        for (text, route) in self.test_messages_with_locations:
+            message = text % route
+            tweet = FakeTweet(self.at_reply + message)
             result = self.wmb.process_tweet(tweet)
+            
+            for unwanted in ('<>', '#', '\[DLR\]', '>T<'):                
+                self.assertNotRegexpMatches(result, unwanted)
+
+            self.assertRegexpMatches(result, route.upper())
+            self.assertRegexpMatches(result, '((Hoxton Station).* to .* [0-9]{4}|None shown going)')
+
+            dm = FakeDirectMessage(message)
+            result = self.wmb.process_tweet(dm)
             
             for unwanted in ('<>', '#', '\[DLR\]', '>T<'):                
                 self.assertNotRegexpMatches(result, unwanted)
