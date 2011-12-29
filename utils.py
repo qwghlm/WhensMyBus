6aff6a8d11e7979982b72212e983cebf9871def0
Chris Applegate <github@qwghlm.co.uk>
2011-12-29 20:00:54 +0000

    Cache-enabled browsing

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ import logging
 import os
 import sqlite3
 import sys
+import time
 import urllib2
 import tweepy
 import ConfigParser
@@ -30,36 +31,53 @@ def load_database(dbfilename):
 
 # JSON stuff
 
-def fetch_json(url, exception_code='tfl_server_down'):
+class WMBBrowser:
     """
-    Fetches a JSON URL and returns Python object representation of it
+    A simple JSON fetcher with caching. Not designed to be used for many thousands of URLs, or for concurrent access
     """
-    opener = urllib2.build_opener()
-    opener.addheaders = [('User-agent', 'When\'s My Transport?'),
-                         ('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')]
+    def __init__(self):
+        """
+        Start up
+        """
+        self.opener = urllib2.build_opener()
+        self.opener.addheaders = [('User-agent', 'When\'s My Transport?'),
+                                  ('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')]
+        logging.debug("Starting up browser")
+        
+        self.cache = {}
     
-    logging.debug("Fetching URL %s", url)
-    try:
-        response = opener.open(url)
-        json_data = response.read()
 
-    # Handle browsing error
-    except urllib2.HTTPError, exc:
-        logging.error("HTTP Error %s reading %s, aborting", exc.code, url)
-        raise WhensMyTransportException(exception_code)
-    except Exception, exc:
-        logging.error("%s (%s) encountered for %s, aborting", exc.__class__.__name__, exc, url)
-        raise WhensMyTransportException(exception_code)
-
-    # Try to parse this as JSON
-    if json_data:
-        try:
-            obj = json.loads(json_data)
-            return obj
-        # If the JSON parser is choking, probably a 503 Error message in HTML so raise a ValueError
-        except ValueError, exc:
-            logging.error("%s encountered when parsing %s - likely not JSON!", exc, url)
-            raise WhensMyTransportException(exception_code)  
+    def fetch_json(self, url, exception_code='tfl_server_down'):
+        """
+        Fetches a JSON URL and returns Python object representation of it
+        """
+        if url in self.cache and (time.time() - self.cache[url]['time']) < 30:
+            logging.debug("Using cached URL %s", url)
+            json_data = self.cache[url]['data']
+            
+        else:
+            logging.debug("Fetching URL %s", url)
+            try:
+                response = self.opener.open(url)
+                json_data = response.read()
+                self.cache[url] = { 'data' : json_data, 'time' : time.time() }
+            # Handle browsing error
+            except urllib2.HTTPError, exc:
+                logging.error("HTTP Error %s reading %s, aborting", exc.code, url)
+                raise WhensMyTransportException(exception_code)
+            except Exception, exc:
+                logging.error("%s (%s) encountered for %s, aborting", exc.__class__.__name__, exc, url)
+                raise WhensMyTransportException(exception_code)
+    
+        # Try to parse this as JSON
+        if json_data:
+            try:
+                obj = json.loads(json_data)
+                return obj
+            # If the JSON parser is choking, probably a 503 Error message in HTML so raise a ValueError
+            except ValueError, exc:
+                logging.error("%s encountered when parsing %s - likely not JSON!", exc, url)
+                raise WhensMyTransportException(exception_code)  
 
 # OAuth stuff
 
