0560abccd8356a8f6bfd1a0d813d430fffbf011a
Chris Applegate <github@qwghlm.co.uk>
2012-02-27 08:33:05 +0000

    Add fixmes

--- lib/browser.py
+++ lib/browser.py
@@ -63,6 +63,7 @@ class WMTBrowser:
                 return obj
             # If the JSON parser is choking, probably a 503 Error message in HTML so raise a ValueError
             except ValueError, exc:
+                # FIXME Delete this from the cache
                 logging.error("%s encountered when parsing %s - likely not JSON!", exc, url)
                 raise WhensMyTransportException(exception_code)  
 
@@ -83,6 +84,6 @@ class WMTBrowser:
                             elem.tag = elem.tag[len(namespace):]
                 return tree
             except Exception, exc:
-                print 
+                # FIXME Delete this from the cache
                 logging.error("%s encountered when parsing %s - likely not XML!", exc, url)
                 raise WhensMyTransportException(exception_code)
--- lib/stringutils.py
+++ lib/stringutils.py
@@ -12,13 +12,13 @@ def capwords(phrase):
     """
     Capitalize each word in a string. A word is defined as anything with a space separating it from the next word.   
     """
-    not_to_be_capitalized = ('via', 'CX')
+    not_to_be_capitalized = ('via', 'CX') # FIXME Needs to be case-insensitive matching - split into lower/upper?
     capitalized = ' '.join([s in not_to_be_capitalized and s or s.capitalize() for s in phrase.split(' ')])
     return capitalized
         
 def cleanup_name_from_undesirables(name, undesirables):
     """
-    Clean out every word in the iterable undesirables from the name supplied, and capitalize
+    Clean out every regular expression in the iterable undesirables from the name supplied, and capitalize
     """
     for undesirable in undesirables:
         name = re.sub(undesirable, '', name, flags=re.I)
