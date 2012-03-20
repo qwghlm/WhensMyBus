ffae6b9684d1828a64bfc3cd8e236b4e5ce2391f
Chris Applegate <github@qwghlm.co.uk>
2012-03-20 17:34:29 +0000

    Todo done :)

--- whensmytransport.py
+++ whensmytransport.py
@@ -19,7 +19,6 @@ Released under the MIT License
 
 Things to do:
 
- - Fully merge WhensMyTube and WhensMyDLR, test thoroughly
  - Add corpus of Tube line & station names so we can better "guess" ambiguous messages
  - Deduces line needed for Tube/DLR if need be
  - Handle inputs based on direction
